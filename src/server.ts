359a9ce02c319dc426208458caba04f453eb64a8
Rinel <rineliniguezsosa@gmail.com>
2024-10-13 20:31:37 -0600

    [FEAT] Cambios en el archivo server.ts
    - importando los cors
    - estableciendo el middleware de cors para restringir otras solicitudes de origen

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(3000,()=>{
     console.log('This server is running in the por 3000');
