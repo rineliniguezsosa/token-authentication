c805d7e841a5da0f95891ffc608532c21a8ab884
Rinel <rineliniguezsosa@gmail.com>
2024-10-13 20:49:18 -0600

    [FEAT] Cambios en el archvo principal server.ts
    - importando las rutas de usuarioRouter
    - estableciendo los middleware para leer la entrada de las rutas de la "/api"
    - creando el middleware con la ruta "/usuario"

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { usuarioRouter } from './routes';
 import cors from 'cors';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/api')
+app.use('/usuario',usuarioRouter)
 
 app.listen(3000,()=>{
     console.log('This server is running in the por 3000');
