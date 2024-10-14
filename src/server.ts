2240e923e97cd6d2497948e477e6f94bf976315b
Rinel <rineliniguezsosa@gmail.com>
2024-10-13 22:28:41 -0600

    [FEAT] Cambios en el archivo server.ts
    - importando el método connectdb
    - eliminando la ruta "/api"
    - llamando al método connectdb para conectar a la base de datos de mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { usuarioRouter } from './routes';
+import { connectdb } from './config';
 import cors from 'cors';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api')
-app.use('/usuario',usuarioRouter)
+app.use('/api/usuario',usuarioRouter)
+
+connectdb();
 
 app.listen(3000,()=>{
     console.log('This server is running in the por 3000');
