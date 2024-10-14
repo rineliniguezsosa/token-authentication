61507d4af35c31e31fbe6a1b97c83caf6615c1b9
Rinel <rineliniguezsosa@gmail.com>
2024-10-14 15:08:11 -0600

    [FEAT] Cambios en el archivo usuario.controller.ts
    - importando el modelo usuarioModel
    - creando la variable newuser donde creamos una nueva instancia de usuarioModel y le pasamos el cuerpo del la data del usuario
    - creamos la variable usuario donde esperamos la respuesta del método save() para guardar la información
    - pasamos en la respuesta del servidor el valor de la respuesta del método save en formato json

--- src/controllers/usuario/usuario.controller.ts
+++ src/controllers/usuario/usuario.controller.ts
@@ -1,12 +1,17 @@
 import { Request,Response } from 'express';
+import { usuarioModel } from '../../models';
+
 
 export const postuser = async(req:Request,resp:Response) =>{
 
     const body = req.body;
+    const newuser = new usuarioModel(body);
+
+    const usuario = await newuser.save();
 
     resp.json({
         status:true,
-        message:body
+        message:usuario
     })
 }
 
