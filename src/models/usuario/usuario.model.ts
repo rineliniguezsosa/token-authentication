9caeead7c18ca06d9f5e12455235d478f3a322c0
Rinel <rineliniguezsosa@gmail.com>
2024-10-14 14:52:13 -0600

    [FEAT] Multiples cambios en el archivo usuario.model.ts
    - desestructurando el Schema y el model de mongoose
    - creando el Schema usuarioSchema donde establecemos los columnas, nombre, correo, password, img, rol, status y google
    - creando la variable usuarioModal donde creamos el modelo con el nombre de Usuario y pasamos el schema para que transforme en un modelo

---
+++ src/models/usuario/usuario.model.ts
@@ -0,0 +1,35 @@
+import { Schema,model } from 'mongoose';
+
+const usuarioSchema =  new Schema({
+    nombre:{
+        type:String,
+        required:[true,'El nombre es obligatorio']
+    },
+    correo:{
+        type:String,
+        required:[true,'El correo es obligatorio'],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,'La contraseña es obligatoria']
+    },
+    img:{
+        type:String
+    },
+    rol:{
+        type:String,
+        required:true,
+        enum:['ADMIN-ROLE','USER_ROLE']
+    },
+    status:{
+        type:Boolean,
+        default:true,
+    },
+    gooogle:{
+        type:Boolean,
+        default:false
+    }
+})
+
+export const usuarioModel = model('Usuario',usuarioSchema);
