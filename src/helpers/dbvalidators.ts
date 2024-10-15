0445fa11bcc670d6b826ee82b0a6564386754664
Rinel <rineliniguezsosa@gmail.com>
2024-10-14 21:36:18 -0600

    [FEAT] Creando el método existEmail para validar el email, pegando la validación del email en el archivo dbvalidators

--- src/helpers/dbvalidators.ts
+++ src/helpers/dbvalidators.ts
@@ -1,9 +1,17 @@
-import { rolModel } from "../models";
+import { rolModel,usuarioModel } from "../models";
 
 export const existRol = async(rol = '') =>{
     const existeRol = await rolModel.findOne({rol});
 
     if(!existeRol){
         throw new Error(`Este ${rol} no existe en la base de datos`);
+    }
+}
+
+export const existEmail = async(correo = '') =>{
+    //verify the email
+    const existEmail = await usuarioModel.findOne({correo});
+    if(existEmail){
+        throw new Error(`El usuario con el ${correo} ya existe`)
     }
 }
