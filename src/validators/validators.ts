b512a788a4b047766c3bf3cb0c326a829b351a39
Rinel <rineliniguezsosa@gmail.com>
2024-10-14 21:05:02 -0600

    [FEAT] Cambios en las validaciones de validateUser
    - importando el método asincrono existRol
    - validando el rol mediante la función custom donde pasamos por referencia el método existRol

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -1,10 +1,12 @@
 import { body } from 'express-validator';
 import { validaterrors } from '../middlewares';
+import { existRol } from '../helpers';
 
 export const validateUser = [
     body('nombre','').trim().not().isEmpty(),
     body('password').trim().isLength({min:6}),
     body('correo').trim().isEmail().withMessage('El campo correo debe de estar en el formato email'),
     body('rol').isIn(['ADMIN-ROLE','USER_ROLE']),
+    body('rol').custom(existRol),
     validaterrors
 ]
