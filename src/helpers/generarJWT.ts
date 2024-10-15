aebc957a00419548119731ac27c7974d2522d8ff
Rinel <rineliniguezsosa@gmail.com>
2024-10-15 17:45:03 -0600

    [FEAT] Cambios en el método generarJWT
    - importando dotenv y importando las variables de entorno
    - retornamos una Promesa donde recibimos las funciones resolve y reject donde creamos la variable payload para almacenar el uid ó el id y donde creamos el token con nuestro payload, nuestra clave secreta y la expiración del token
    - si tenemos error en crear el token estableci el log para ver el error y llame a la función reject pasandole el mensaje del error y si tood sale adecuadamente llame al método resolve con el valor del token

--- src/helpers/generarJWT.ts
+++ src/helpers/generarJWT.ts
@@ -1,5 +1,19 @@
 import jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
 
-export const generarJWT = async() =>{
+dotenv.config();
 
+export const generarJWT = async(uid: string  = '') =>{
+    return new Promise((resolve,reject)=>{
+        const payload = {uid};
+
+        jwt.sign(payload,`${process.env.SECRETKEY}`,{expiresIn:'4h'},(error,token)=>{
+            if (error) {
+                console.log(error);
+                reject('No se puedo generar el token')
+            }else{
+                resolve(token)
+            }
+        })
+    })
 }
