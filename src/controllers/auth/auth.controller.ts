153dfbcc018836de504565deada297e149e2c3cc
Rinel <rineliniguezsosa@gmail.com>
2024-10-15 17:41:13 -0600

    [FEAT] Multiples cambios en el método Login
    - importando el modelo usuarioModel
    - importando bcrypt
    - importando el método que genera el token gnerarJWT
    - desestructuramos el request body el correo y el password
    - establecemos el bloque try catch
    - creamos la variable existUsario donde esperamos la respuesta del método findOne donde estamos buscando a un usuario por email
    - creando el bloque if para validar la respuesta de la variable existUsuario donde si no existe el usuario devolveremos un status 400 con el mensaje en formato json status false y el message como "Usuario / Password no son correctos"
    - creamos otro bloque if para validar  el status del usuario donde si la resopuesta es false devolveremos la respuesta como un status 400 y el formato en json con el valor status en false y con el message de Usuario / Password no son correctos - status:false
    - creamos la variable verifypassword donde usamos bcrypt para verificar el password con el hash y el password que manda el usuario en el body y si este es false devovleremos el mensaje con el status 400 con el fomato en json de el status en false y el message en El Password no es correcto
    - creamos la variable token donde esperamos la respuesta del método generarJWT donde pasamos por argumento el id del usuario por almacenar en el token
    - si todo sale bien devolvemos la respuesta del usuario almacenada en existUsuario y el token
    - si el método falla entonces devolveremos un log y la respuesta del servidor

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,5 +1,47 @@
 import { Request,Response } from 'express';
+import { usuarioModel } from '../../models';
+import bcrypt from 'bcrypt';
+import { generarJWT } from '../../helpers';
 
 export const Login = async(req:Request,resp:Response) =>{
+    const { correo,password } = req.body;
+    try {
+        //verifica si el email existe
+        const existUsuario = await usuarioModel.findOne({correo})
+        if(!existUsuario){
+            return resp.status(400).json({
+                status:false,
+                message:"Usuario / Password no son correctos"
+            })
+        }
+        //si el usuario esta activo
+        if(!existUsuario.status){
+            return resp.status(400).json({
+                status:false,
+                message:"Usuario / Password no son correctos - status:false"
+            })
+        }
+        //verificar el password
+        const verifypassword = bcrypt.compareSync(password,existUsuario.password);
+        if(!verifypassword){
+            return resp.status(400).json({
+                status:false,
+                message:"El Password no es correcto"
+            })
+        }
+        //generar el JWT
+        const token = await generarJWT(existUsuario.id)
 
+        resp.json({
+            existUsuario,
+            token
+        })
+    } catch (error) {
+        console.log(error);
+        return resp.json({
+            status:false,
+            message:'Algo salio mal, volver a intentar'
+        })
+        
+    }
 }
