a0313cdce93d1af8d901d1247b9a9c51a1cec53f
Rinel <rineliniguezsosa@gmail.com>
2024-10-14 21:52:26 -0600

    [FEAT] Creando la ruta con el param ""/:id" para actualizar los datos del usuario
    - importando el controlador updateUser

--- src/routes/usuario/usuario.routes.ts
+++ src/routes/usuario/usuario.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { postuser,getuser } from '../../controllers';
+import { postuser,getuser, updateUser } from '../../controllers';
 import { validateUser } from '../../validators';
 
 export const usuarioRouter = Router();
 
 usuarioRouter.post('/post',validateUser,postuser)
+usuarioRouter.put('/:id',updateUser)
 usuarioRouter.get('/get',getuser)
