c196b0669a80b20481279c076b2df9981f93fc98
Rinel <rineliniguezsosa@gmail.com>
2024-10-14 19:37:42 -0600

    [FEAT] Cambios en el archivo usuario.roiutes
    - importando el validateuser
    - estableciendo el array validateuser como middleware de la ruta "/post" para validar las entradas del usuario

--- src/routes/usuario/usuario.routes.ts
+++ src/routes/usuario/usuario.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { postuser,getuser } from '../../controllers';
+import { validateUser } from '../../validators';
 
 export const usuarioRouter = Router();
 
-usuarioRouter.post('/post',postuser)
+usuarioRouter.post('/post',validateUser,postuser)
 usuarioRouter.get('/get',getuser)
