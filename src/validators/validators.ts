fbf9214dd3b37265255b89b8edfc989071f2125b
Rinel <rineliniguezsosa@gmail.com>
2024-10-14 20:06:18 -0600

    [FEAT] Cambios en el archivo validators
    - importando validaterrors
    - validando las entradas de los inputs, nombre, password, correo y el rol
    - estableciendo la función validaterrors para comprobar los errores en las entradas

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -1,5 +1,10 @@
 import { body } from 'express-validator';
+import { validaterrors } from '../middlewares';
 
 export const validateUser = [
-    body('correo').isEmail().withMessage('El campo correo debe de estar en el formato email'),
+    body('nombre','').trim().not().isEmpty(),
+    body('password').trim().isLength({min:6}),
+    body('correo').trim().isEmail().withMessage('El campo correo debe de estar en el formato email'),
+    body('rol').isIn(['ADMIN-ROLE','USER_ROLE']),
+    validaterrors
 ]
