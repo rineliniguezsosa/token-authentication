c444f0c177d8f113d27f1cbf3661a9b04974ca3d
Rinel <rineliniguezsosa@gmail.com>
2024-10-15 21:50:31 -0600

    [FEAT] Cambios en el archivo validar-roles.ts
    - creando el método asincrono tieneRol
    - retornaremos una función con los parametros de request, response y next
    - comprobaremos si no existe un usuario en el request body devolveremos un error del servidor
    - creamos el bloque if para validar los tipos de roles que no tiene el usuario donde realizamos el método includes para revisar el rol del request body y si no se encunetra en la lista de los roles se los devolveremos al usuario para que el sepa que tipo de roles necesita para eliminar en este caso
    - devolvemos la función next para que sigamos con las validaciones

--- src/middlewares/validar-roles.ts
+++ src/middlewares/validar-roles.ts
@@ -17,3 +17,23 @@ export const isAdminrol = async(req:Request,resp:Response,next:NextFunction) =>{
 
     next()
 }
+
+export const tieneRol = async(...roles:string[]) =>{
+    return (req:Request,resp:Response,next:NextFunction) =>{
+        if(!req.usuario){
+            return resp.status(500).json({
+                status:false,
+                message:'Se quiere verificar el rol sin validar el token primero'
+            })
+        }
+
+        if (!roles.includes(req.usuario.rol)) {
+            return resp.status(500).json({
+                status:false,
+                message:`El servicio requiere uno de estos roles ${roles}`
+            })
+        }
+
+        next();
+    }
+}
