7321fca35c957dce865a8a5e8b86745147d27eb9
Rinel <rineliniguezsosa@gmail.com>
2024-10-15 21:01:51 -0600

    [FEAT] Multiples cambios en mi archivo validar-jwt
    - importando JwtPayload  de jsonwebtoken
    - importando dotenv
    - importando el modelo usuarioModel
    - creando la interface CustomJwtPayload para tipar el payload del jsonwebtoken
    - declarando globalmente el namespace para extender la interfaz de Request de express y tipar el uid de tipo string
    - creando el método validarjwtinRoutes  para validar que tengas un usuario valido en la base de datos
    - creamos la variable token para obtener del request el token
    - creamos el bloque if para verificar el token y si no existe devolver un error
    - establecemos el bloque try catch para el caso en el que si exista un token
    - desestructuramos el uid de la verificación del token para saber si es un token valido y que no este manipulado
    - creamos la variable usuario para verificar que ese uid sea de un usuario valido en la base de datos
    - creamos el bloque if para comprobar si no existe el usuariio devolvemos un error
    - creamos un bloque if para comprobar que el status de un usuario sea diferente a false ó que el usuario este activo entonces devolvemos el error
    - del request.body obtenemos la propiedad usuario y se la asignamos a los datos del usuario
    - establecemos la función next para seguir con los middlewares
    - en la sección del catch devolvemos un log del error y devolvemos un error para el usuario

--- src/middlewares/validar-jwt.ts
+++ src/middlewares/validar-jwt.ts
@@ -1,2 +1,59 @@
 import {Request,Response,NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload }  from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+import { usuarioModel } from '../models';
+
+dotenv.config();
+
+interface CustomJwtPayload extends JwtPayload {
+    uid: string; // Asegúrate de que el payload contiene la propiedad `uid`
+}
+
+declare global {
+    namespace Express {
+      interface Request {
+        usuario?: any;  // uid será de tipo string o undefined
+      }
+    }
+}
+
+export const validarjwtinRoutes = async(req:Request,resp:Response,next:NextFunction) =>{
+
+    const token = req.header('token');
+    if(!token){
+        return resp.status(401).json({
+            status:false,
+            message:'No existe el token en la petición'
+        })
+    }
+    
+    try {
+        const { uid } = jwt.verify(token,`${process.env.SECRETKEY}`) as CustomJwtPayload
+
+        //validar el usuario que corresponde con el uid
+        const usuario = await usuarioModel.findById(uid);
+
+        if(!usuario){
+            return resp.status(401).json({
+                status:false,
+                message:"El usuario no existe"
+            })
+        }
+        // verificar el status en true 
+        if(!usuario?.status){
+            return resp.status(401).json({
+                status:false,
+                message:"Token no valido - status en false"
+            })
+        }
+        req.usuario = usuario;
+        
+        next()
+    } catch (error) {
+        console.log(error);
+        resp.status(401).json({
+            status:false,
+            message:'Token no valido'
+        })
+    }
+}
