43a73844c2540b1df9d3d437d713aea1217d15d7
Rinel <rineliniguezsosa@gmail.com>
2024-10-18 18:06:14 -0600

    [FEAT] Estableciendo la url de el origen en crudo el cual aceptara peticiones ó solicitudes en nuestra api

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:process.env.FRONTENDURL
+    origin:'http://localhost:5173',
 }));
 app.use('/api/usuario',usuarioRouter)
 app.use('/auth',authRouter)
