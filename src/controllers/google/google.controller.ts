01db89d3426802db36635e4ca177a2929aee502d
Rinel <rineliniguezsosa@gmail.com>
2024-10-18 22:34:15 -0600

    [FEAT] Multiples cambios en el controlador googleLogin
    -  importando el helper generarJWT
    - importando el modelo usuarioModel
    - creando la interface GoogleTokenPayload  donde tipamos las props name, email y picture como strings
    - desestructurando el name, el email y el picture de el método googleverify y lo tipamos por la interface oogleTokenPayload
    - creando la variable usuario donde esperamos la respuesta del método findone para buscar un usuario por correo
    - creamos el bloque if donde validamos si el usuario no existe, entonces procedemos a crear la data para crear un nuevo usuario en la base de datos
    - creamos una nueva instancia de usuarioModel y la pasamos el objeto data para creal el objeto
    - esperamos la respuesat del método save para guardar el nuevo usuario en la base de datos
    - creamos el bloque if para verificar el status del usuario y si es true entonces devolvemos un mensaje de servisdor
    - creamos la variable token donde esperamos la respuesta del método generarJWT y donde pasamos el payload del id del usuario
    - devolvemos al usuario y el token en la respuesta del servidor

--- src/controllers/google/google.controller.ts
+++ src/controllers/google/google.controller.ts
@@ -1,16 +1,49 @@
 import { Request,Response } from 'express';
-import { googleverify } from '../../helpers';
+import { generarJWT, googleverify } from '../../helpers';
+import { usuarioModel } from '../../models';
+
+interface GoogleTokenPayload {
+    name: string;
+    email: string;
+    picture: string;
+  }
 
 export const googleLogin = async(req:Request,resp:Response) =>{
     const { id_token } = req.body;
     
     try {    
-        const google = await googleverify(id_token);
-        console.log("google data: ",google);
+        const { name,email,picture} = await googleverify(id_token) as GoogleTokenPayload;
+
+        let usuario = await usuarioModel.findOne({correo:email})
+
+        if(!usuario){
+            const data = {
+                nombre:name,
+                correo:email,
+                password:'',
+                img:picture,
+                google:true
+            }
+
+            usuario = new usuarioModel(data);
+
+            await usuario.save()
+        }
+
+        //vericar el status del usuario
+
+        if (!usuario.status) {
+            resp.status(401).json({
+                status:false,
+                message:'Hable con el administrador'
+            })
+        }
+
+        const token = await generarJWT(usuario.id);
         
         resp.json({
-            status:true,
-            message:id_token
+            usuario,
+            token     
         })
     } catch (error) {
         console.log(error);
