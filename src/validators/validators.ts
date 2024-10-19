6176fcde5a8e945d11bb9d44c039e752cc042b63
Rinel <rineliniguezsosa@gmail.com>
2024-10-18 22:46:33 -0600

    [FEAT] Eliminando en el array validateuser las validaciones  para el campo password ya no sera requerido

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -5,7 +5,7 @@ import { validarjwtinRoutes } from '../middlewares';
 
 export const validateUser = [
     body('nombre','').trim().not().isEmpty(),
-    body('password').trim().isLength({min:6}).not().isEmpty(),
+    body('password').trim().isLength({min:6}),
     body('correo').trim().isEmail().withMessage('El campo correo debe de estar en el formato email').custom(existEmail),
     // body('rol').isIn(['ADMIN-ROLE','USER_ROLE']),
     body('rol').custom(existRol),
